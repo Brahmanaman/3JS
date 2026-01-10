9fbf29974b740d4aacee4d438e21fb6c9e2d2861
Aman Sharma <sharma9917375201@gmail.com>
2026-01-10 22:57:04 +0530

    added responsive and orbitControls

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 //created a scene
 const scene = new THREE.Scene();
@@ -11,7 +12,7 @@ camera.position.z = 5;
 //created a geometry(object)
 const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
 //created a material
-const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x00FF00 })
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x00FF00, wireframe: true })
 //created a Mesh
 const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 scene.add(cube)
@@ -24,15 +25,31 @@ light.position.z = 5;
 
 //created a renderer
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(innerWidth, innerHeight);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.autoRotate = true;
+controls.autoRotateSpeed = 12.0
+controls.enableZoom = true
+controls.dampingFactor = 0.25
+
 //added renderer to the body
 document.body.appendChild(renderer.domElement);
 
+//this make the responsive
+window.addEventListener("resize", () => {
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    camera.aspect = window.innerWidth / window.innerHeight; // jab jab camera ki position update hoti hai, to projection ko update karna hota hai
+    camera.updateProjectionMatrix();
+})
+
 function animate() {
-    cube.rotation.x += 0.1
-    cube.rotation.y += 0.1
+    // cube.rotation.x += 0.01
+    // cube.rotation.y += 0.01
     // cube.rotation.z = 0.4
     renderer.render(scene, camera);
+    controls.update();
 }
 
 renderer.setAnimationLoop(animate)
